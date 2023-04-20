13b4637c73846330852b13d1deebe59aa849ceab
ni3choudhary <chnitin012@gmail.com>
2023-04-20 16:04:16 +0530

    Added data.py which is used to generate fake data using Faker Library

---
+++ data.py
@@ -0,0 +1,36 @@
+# Import Faker Library
+from faker import Faker
+
+# create and initialize a faker generator 
+faker = Faker()
+
+# predefined list of groceries words
+items_list  = ["Soda", "Milk", "Chips","Eggs","Breads","Breakfast","Beer","Water"]
+
+def get_user_data():
+    """
+    Generating fake data using Faker library
+    """
+    return {
+        "name": faker.name(),
+        "address": faker.address(),
+        "created_at": faker.year(),
+        "phone" : faker.phone_number(),
+        "items":','.join([str(elem) for elem in faker.words(3, True, items_list)]),
+        "total_cost": faker.random_int(10, 100)
+    }
+
+
+# to remove whitespace from string 
+def remove_whitespaces(string):
+    return "".join(string.split())
+
+# final data with email address
+def get_registered_user():
+    data = get_user_data()
+    data['email'] = f"{remove_whitespaces(data['name'])}@gmail.com"
+    return data
+
+
+if __name__ == "__main__":
+    print(get_registered_user())
